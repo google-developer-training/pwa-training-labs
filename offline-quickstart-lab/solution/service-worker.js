19e5216f8164b071ea09a2f96760b260c57defdc
David Scales <dscales@google.com>
2016-11-01 11:21:31 -0700

    offline-quickstart lab: pull code into function

--- offline-quickstart-lab/solution/service-worker.js
+++ offline-quickstart-lab/solution/service-worker.js
@@ -19,26 +19,26 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
     .then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request)
-      .then(function(response) {
-        // Check if we received a valid response
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-        return response;
-      })
-      .catch(function(error) {
-        console.log('Request failed:', error);
-        // You could return a custom offline 404 page here
-      });
+      return response || fetchAndCache(event.request);
     })
   );
 });
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
