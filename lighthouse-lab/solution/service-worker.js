e330d134d29a18dd2f58944dcad0a93d81305a86
David Scales <dscales@google.com>
2016-11-30 14:54:15 -0800

    lighthouse: small code clean up

--- lighthouse-lab/solution/service-worker.js
+++ lighthouse-lab/solution/service-worker.js
@@ -28,13 +28,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request)
   .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-    .catch(function() {
-      console.log('resource not available');
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
