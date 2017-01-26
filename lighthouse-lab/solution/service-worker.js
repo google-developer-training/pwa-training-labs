e470b366f02a5c7c95e965cfdc27f0752d3d24db
David Scales <dscales@google.com>
2017-01-26 10:19:09 -0800

    lighthouse: fix spacing

--- lighthouse-lab/solution/service-worker.js
+++ lighthouse-lab/solution/service-worker.js
@@ -15,20 +15,20 @@ limitations under the License.
 */
 self.addEventListener('install', function(event) {
   event.waitUntil(
-      caches.open('static-cache-v1')
-        .then(function(cache) {
-          return cache.addAll([
-            '.',
-            'index.html',
-            'css/main.css',
-            'http://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-            'images/still_life-1600_large_2x.jpg',
-            'images/still_life-800_large_1x.jpg',
-            'images/still_life_medium.jpg',
-            'images/still_life_small.jpg'
-          ]);
-        })
-    );
+    caches.open('static-cache-v1')
+    .then(function(cache) {
+      return cache.addAll([
+        '.',
+        'index.html',
+        'css/main.css',
+        'http://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+        'images/still_life-1600_large_2x.jpg',
+        'images/still_life-800_large_1x.jpg',
+        'images/still_life_medium.jpg',
+        'images/still_life_small.jpg'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
