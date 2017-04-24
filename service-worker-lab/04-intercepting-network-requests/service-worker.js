7905b09833dc7d456136b8e9a6a96bc77ecebf59
David Scales <dscales@google.com>
2017-04-24 16:06:25 -0700

    feat(service-worker): add skipWaiting exercise
    Added a new section (section 3.4) for student to
    use skipWaiting() method.
    
    Change-Id: I5405a41daf54d287831d26cabec57f02a0b0b64a

--- service-worker-lab/04-intercepting-network-requests/service-worker.js
+++ service-worker-lab/04-intercepting-network-requests/service-worker.js
@@ -18,13 +18,14 @@ limitations under the License.
 
   self.addEventListener('install', function(event) {
     console.log('Service worker installing...');
+    self.skipWaiting();
   });
 
   self.addEventListener('activate', function(event) {
     console.log('Service worker activating...');
   });
 
-  // A comment ;)
+  // I'm a new service worker
 
   self.addEventListener('fetch', function(event) {
     console.log('Fetching:', event.request.url);
