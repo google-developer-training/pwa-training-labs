33ce23cefd24e31745021aac1a144d937cdde92f
Nicolas Searle <nsearle@google.com>
2017-10-05 16:24:16 -0700

    add a migrating from sw-precache to Workbox lab

---
+++ sw-precache-workbox-lab/README.md
@@ -0,0 +1,35 @@
+# sw-precache/toolbox to Workbox
+
+In this lab you learn how to cpnvert a site using sw-precache and sw-toolbox to
+to one that uses Workbox.
+
+## Getting started
+
+Clone the repository and navigate to **sw-precache-workbox-lab/app**.
+
+Start a local web server at the app base directory. Then, open your browser and
+navigate to the appropriate local host port (for example, http://localhost:8080/).
+
+[Download and install Node.js](https://nodejs.org/en/download/).
+
+## Note
+
+Some contents from Responsive Blog Sample - Copyright Sam Dutton 2016
+
+## License
+
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This is not an official Google product.
---
+++ sw-precache-workbox-lab/solution/gulpfile.js
@@ -0,0 +1,61 @@
+/*jshint esversion:6*/
+/**
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const del = require('del');
+const runSequence = require('run-sequence');
+const wbBuild = require('workbox-build');
+
+// Clean build directory
+gulp.task('clean', () => del(['.tmp', 'build/*', '!build/.git'], {dot: true}));
+
+gulp.task('copy', () =>
+  gulp.src([
+    'app/**/*'
+  ]).pipe(gulp.dest('build'))
+);
+
+gulp.task('service-worker', () => {
+  return wbBuild.injectManifest({
+    swSrc: 'app/sw.js',
+    swDest: 'build/sw.js',
+    globDirectory: 'build',
+    staticFileGlobs: [
+      'index.html',
+      'css/main.css'
+    ]
+  })
+  .catch((err) => {
+    console.log('[ERROR] This happened: ' + err);
+  });
+});
+
+gulp.task('default', ['clean'], cb => {
+  runSequence(
+    'copy',
+    'service-worker',
+    cb
+  );
+});
+
+gulp.task('serve', ['default'], () => {
+  browserSync.init({
+    server: 'build',
+    port: 8002
+  });
+  gulp.watch('app/*', ['default']).on('change', browserSync.reload);
+});
