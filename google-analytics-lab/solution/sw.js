baba99f119afacbc4f7b6149068539e3acdc554c
David Scales <davidsscales@gmail.com>
2018-02-13 14:59:56 -0800

    fix(analytics-lab): update to gtag.js API & Workbox
    This commit updates the lab to use the new gtag.js API.
    
    It also replaces the Google offline analytics package
    with workbox-google-analytics. Note: this section still
    needs some additional content.
    
    There are also some other miscellaneous & minor refactors.

--- google-analytics-lab/solution/sw.js
+++ google-analytics-lab/solution/sw.js
@@ -14,70 +14,45 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-self.importScripts('js/analytics-helper.js');
+importScripts('js/analytics-helper.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
 
-importScripts(
-  'node_modules/sw-offline-google-analytics/offline-google-analytics-import.js'
-);
-goog.offlineGoogleAnalytics.initialize();
+workbox.googleAnalytics.initialize();
 
-(function() {
-  'use strict';
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  console.log('Closed notification: ' + primaryKey);
+  e.waitUntil(
+    sendAnalyticsEvent('close', 'notification')
+  );
+});
 
-  self.addEventListener('notificationclose', function(e) {
-    var notification = e.notification;
-    var primaryKey = notification.data.primaryKey;
-    console.log('Closed notification: ' + primaryKey);
-    e.waitUntil(
-      sendAnalyticsEvent('close', 'notification')
-    );
-  });
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  notification.close();
+  e.waitUntil(
+    Promise.all([
+      clients.openWindow('pages/page' + primaryKey + '.html'),
+      sendAnalyticsEvent('click', 'notification')
+    ])
+  );
+});
 
-  self.addEventListener('notificationclick', function(e) {
-    var notification = e.notification;
-    var primaryKey = notification.data.primaryKey;
-    notification.close();
-    var action = e.action;
-    if (action === 'delay') {
-      // Notificaion delay logic could go here in a real app
-      e.waitUntil(
-        sendAnalyticsEvent('delay', 'notification')
-      );
-    } else if (action === 'buy') {
-      // Purchase logic could go here in a real app
-      e.waitUntil(
-        sendAnalyticsEvent('purchase', 'notification')
-      );
-    } else {
-      e.waitUntil(
-        Promise.all([
-          clients.openWindow('pages/page' + primaryKey + '.html'),
-          sendAnalyticsEvent('click', 'notification')
-        ])
-      );
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/notification-flat.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '-push-notification'
     }
-  });
-
-  self.addEventListener('push', function(e) {
-    var options = {
-      body: 'This notification was generated from a push!',
-      icon: 'images/notification-flat.png',
-      vibrate: [100, 50, 100],
-      data: {
-        dateOfArrival: Date.now(),
-        primaryKey: '-push-notification'
-      },
-      actions: [
-        {action: 'buy', title: 'Buy on sale!',
-          icon: 'images/checkmark.png'},
-        {action: 'delay', title: 'Not right now',
-          icon: 'images/xmark.png'},
-      ]
-    };
-    e.waitUntil(Promise.all([
-        self.registration.showNotification('Hello world!', options),
-        sendAnalyticsEvent('recieved', 'push')
-      ])
-    );
-  });
-})();
+  };
+  e.waitUntil(Promise.all([
+      self.registration.showNotification('Hello world!', options),
+      sendAnalyticsEvent('received', 'push')
+    ])
+  );
+});
