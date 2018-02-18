57715ddcb9ccb7db9278b15f74831628ca9f20a9
David Scales <davidsscales@gmail.com>
2018-02-17 16:22:55 -0800

    feat(offline-quickstart-lab): new solution code

--- offline-quickstart-lab/solution/service-worker.js
+++ offline-quickstart-lab/solution/service-worker.js
@@ -1,43 +1,50 @@
-var CACHE_NAME = 'static-cache';
-var urlsToCache = [
-  '.',
+/*
+Copyright 2018 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
   'index.html',
-  'styles/main.css'
+  'styles/main.css',
+  'images/space1.jpg',
+  'images/space2.jpg',
+  'images/space3.jpg'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetchAndCache(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
 });
 
-function fetchAndCache(url) {
-  return fetch(url)
-  .then(function(response) {
-    // Check if we received a valid response
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      cache.put(url, response.clone());
-      return response;
-    });
-  })
-  .catch(function(error) {
-    console.log('Request failed:', error);
-    // You could return a custom offline 404 page here
-  });
-}
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
