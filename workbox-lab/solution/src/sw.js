6769db95a44617d2efa090ed26656cdb306f325f
Nicolas Searle <nasearle@ucla.edu>
2018-03-01 14:28:05 -0800

    update(workbox-lab): replace cheese ipsum, fix sw
    This commit replaces cheese ipsum with lorem ipsum to match the news
    theme more. It also moves the service worker routes inside the
    if (workbox) block.

--- workbox-lab/solution/src/sw.js
+++ workbox-lab/solution/src/sw.js
@@ -20,61 +20,61 @@ if (workbox) {
 
   workbox.precaching.precacheAndRoute([]);
 
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
 
-workbox.routing.registerRoute(
-  /(.*)articles(.*)\.(?:png|gif|jpg)/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images-cache',
+  const articleHandler = workbox.strategies.networkFirst({
+    cacheName: 'articles-cache',
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
       })
     ]
-  })
-);
+  });
 
-const articleHandler = workbox.strategies.networkFirst({
-  cacheName: 'articles-cache',
-  plugins: [
-    new workbox.expiration.Plugin({
-      maxEntries: 50,
+  workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+    return articleHandler.handle(args).then(response => {
+      if (!response) {
+        return caches.match('pages/offline.html');
+      } else if (response.status === 404) {
+        return caches.match('pages/404.html');
+      }
+      return response;
+    });
+  });
+
+  const postHandler = workbox.strategies.cacheFirst({
+    cacheName: 'posts-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+      })
+    ]
+  });
+
+  workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
+    console.log('test');
+    return postHandler.handle(args).then(response => {
+      if (response.status === 404) {
+        return caches.match('pages/404.html');
+      }
+      return response;
     })
-  ]
-});
-
-workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
-  return articleHandler.handle(args).then(response => {
-    if (!response) {
+    .catch(function() {
       return caches.match('pages/offline.html');
-    } else if (response.status === 404) {
-      return caches.match('pages/404.html');
-    }
-    return response;
+    });
   });
-});
 
-const postHandler = workbox.strategies.cacheFirst({
-  cacheName: 'posts-cache',
-  plugins: [
-    new workbox.expiration.Plugin({
-      maxEntries: 50,
-    })
-  ]
-});
-
-workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
-  console.log('test');
-  return postHandler.handle(args).then(response => {
-    if (response.status === 404) {
-      return caches.match('pages/404.html');
-    }
-    return response;
-  })
-  .catch(function() {
-    return caches.match('pages/offline.html');
-  });
-});
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
