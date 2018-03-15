aaadd48f80a08fa767faafc87011ca926c29c95e
David Scales <davidsscales@gmail.com>
2018-03-15 13:46:06 -0700

    feat(gulp-lab): new starter app & code

---
+++ gulp-lab/project/app/scripts/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    document.getElementById('main').insertAdjacentHTML(
+      'afterbegin',
+      '<section class="card welcome"><h3>Welcome!</h3></section>'
+    );
+  });
