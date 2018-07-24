b7ea6836bad18d697a261bd4d187cd76ec29a7ee
David Scales <davidsscales@gmail.com>
2018-07-24 13:32:54 -0700

    refactor(workbox-lab): to gulp v4 to fix security issues

--- workbox-lab/solution/gulpfile.js
+++ workbox-lab/solution/gulpfile.js
@@ -16,29 +16,22 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const del = require('del');
-const runSequence = require('run-sequence');
 const workboxBuild = require('workbox-build');
 
 // Clean "build" directory
-gulp.task('clean', () => del(['build/*'], {dot: true}));
+const clean = () => {
+  return del(['build/*'], {dot: true});
+};
+gulp.task('clean', clean);
 
 // Copy "src" directory to "build" directory
-gulp.task('copy', () =>
-  gulp.src(['src/**/*']).pipe(gulp.dest('build'))
-);
-
-// Build the app, run a local dev server, and rebuild on "src" file changes
-gulp.task('serve', ['default'], () => {
-  browserSync.init({
-    server: 'build',
-    port: 8081,
-    open: false
-  });
-  gulp.watch('src/**/*', ['default']).on('change', browserSync.reload);
-});
+const copy = () => {
+ return gulp.src(['src/**/*']).pipe(gulp.dest('build'));
+};
+gulp.task('copy', copy);
 
 // Inject a precache manifest into the service worker
-gulp.task('build-sw', () => {
+const buildSw = () => {
   return workboxBuild.injectManifest({
     swSrc: 'src/sw.js',
     swDest: 'build/sw.js',
@@ -52,16 +45,30 @@ gulp.task('build-sw', () => {
       'pages/offline.html',
       'pages/404.html'
     ]
+  }).then(resources => {
+    console.log(`Injected ${resources.count} resources for precaching, ` +
+        `totaling ${resources.size} bytes.`);
   }).catch(err => {
     console.log('Uh oh 😬', err);
   });
-});
+}
+gulp.task('build-sw', buildSw);
 
-// This is the default task and app's build process
-gulp.task('default', ['clean'], cb => {
-  runSequence(
-    'copy',
-    'build-sw',
-    cb
-  );
+// This is the app's build process
+const build = gulp.series('clean', 'copy', 'build-sw');
+gulp.task('build', build);
+
+// Build the app, run a local dev server, and rebuild on "src" file changes
+const browserSyncOptions = {
+  server: 'build',
+  port: 8081,
+  open: false
+};
+const serve = gulp.series(build, () => {
+  browserSync.init(browserSyncOptions);
+  return gulp.watch('src/**/*', build).on('change', browserSync.reload);
 });
+gulp.task('serve', serve);
+
+// Set the default task to "build"
+gulp.task('default', build);
