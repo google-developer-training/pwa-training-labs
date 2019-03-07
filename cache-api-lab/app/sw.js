b43c38fb578c43d138ba5cf22ff6ad7eb8014de3
Piotr Szypcio <piotr.szypcio@lppsa.com>
2019-03-07 13:55:37 +0100

    ECOM-666 hiper cache feature OMG

---
+++ cache-api-lab/app/sw.js
@@ -0,0 +1,46 @@
+const filesToCache = [
+    '/',
+    'style/main.css',
+    'images/still_life_medium.jpg',
+    'index.html',
+    'pages/offline.html',
+    'pages/404.html'
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+        caches.open(staticCacheName)
+              .then(cache => {
+                  return cache.addAll(filesToCache);
+              })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+              .then(response => {
+                  if (response) {
+                      console.log('Found ', event.request.url, ' in cache');
+                      return response;
+                  }
+                  console.log('Network request for ', event.request.url);
+                  return fetch(event.request)
+                  .then(response => {
+                      if (404 === response.status) {
+                          return caches.match('pages/404.html');
+                      }
+                      return caches.open(staticCacheName).then(cache => {
+                          cache.put(event.request.url, response.clone());
+                          return response;
+                      });
+                  });
+              }).catch(error => {
+                return caches.match('pages/offline.html');
+        })
+    );
+});
