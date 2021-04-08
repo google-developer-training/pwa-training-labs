8bd0cd55f7f21cd859f79203b7c56a7e5bbe3d28
Marcio <oliveira.c.marcio@gmail.com>
2021-04-08 18:03:04 -0300

    final commit, IndexedDB created

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -298,7 +298,11 @@ var idbApp = (function() {
 
   function getOrders() {
 
-    // TODO 5.4 - get all objects from 'orders' object store
+      return dbPromise.then(function(db) {
+        var tx = db.transaction('orders','readonly')
+        var store = tx.objectStore('orders')
+        return store.getAll()
+      })
 
   }
 
@@ -312,21 +316,48 @@ var idbApp = (function() {
 
   function processOrders(orders) {
 
-    // TODO 5.5 - get items in the 'products' store matching the orders
+    return dbPromise.then(function(db) {
+      var tx = db.transaction('products');
+      var store = tx.objectStore('products');
+      return Promise.all(
+        orders.map(function(order) {
+          return store.get(order.id).then(function(product) {
+            return decrementQuantity(product, order);
+          });
+        })
+      );
+    });
 
   }
 
   function decrementQuantity(product, order) {
 
-    // TODO 5.6 - check the quantity of remaining products
+    return new Promise(function(resolve, reject) {
+      var item = product;
+      var qtyRemaining = item.quantity - order.quantity;
+      if (qtyRemaining < 0) {
+        console.log('Not enough ' + product.id + ' left in stock!');
+        document.getElementById('receipt').innerHTML =
+        '<h3>Not enough ' + product.id + ' left in stock!</h3>';
+        throw 'Out of stock!';
+      }
+      item.quantity = qtyRemaining;
+      resolve(item);
+    });
 
   }
 
   function updateProductsStore(products) {
     dbPromise.then(function(db) {
 
-      // TODO 5.7 - update the items in the 'products' object store
-
+      var tx = db.transaction('products', 'readwrite')
+      var store = tx.objectStore('products')
+      return Promise.all(products.map((item) => {
+        return store.put(item)
+      })).catch((error) => {
+        tx.abort(error)
+        console.log(error)
+      })
     }).then(function() {
       console.log('Orders processed successfully!');
       document.getElementById('receipt').innerHTML =
